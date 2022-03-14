dbb151cd685cf9b5fb77c3ac5e1f7fbdf3150a3d
NehuGumber91 <gillpag102@gmail.com>
2022-03-14 20:42:37 -0300

    new feature

--- app.js
+++ app.js
@@ -166,4 +166,101 @@ app.get("/atm", async (req, res) => {
   }
 });
 
+app.get("/marks", async (req, res) => {
+  await client.connect();
+  const Maindb = client.db("MainDatabase");
+  const db = client.db("Taxonomy");
+  var run = 0;
+  var next_url =
+    baseurl +
+    "/Mediapartners/IRqNxUD5KguY3257508WYgzweAZuY8UPj1/Catalogs/4021/Items?PageSize=400";
+  while (run < 88) {
+    console.log(next_url);
+    await fetch(next_url, {
+      method: "GET",
+      headers: {
+        Authorization:
+          "Basic " +
+          base64.encode(
+            "IRqNxUD5KguY3257508WYgzweAZuY8UPj1" +
+              ":" +
+              "tb-Byb6gtJv~rCAPCyjNrhdeLPKHVFY8"
+          ),
+        Accept: "application/json",
+      },
+    }).then(async (response) => {
+      await response.json().then(async (data) => {
+        let items = data.Items;
+        var i = 0;
+        console.log("after");
+        console.log(data["@nextpageuri"]);
+        next_url = await (baseurl + data["@nextpageuri"]);
+        for (i == 0; i < items.length; i++) {
+          console.log("Started" + items[i].CatalogItemId);
+          let category = await items[i].Category;
+          var tax_item = await db
+            .collection("taxonomy")
+            .aggregate([
+              {
+                $search: {
+                  index: "default",
+                  text: {
+                    query: category,
+                    path: {
+                      wildcard: "*",
+                    },
+                  },
+                },
+              },
+              { $limit: 1 },
+            ])
+            .toArray();
+          let sendingdata = {
+            CatalogId: items[i].CatalogId,
+            CampaignId: items[i].CampaignId,
+            CampaignName: items[i].CampaignName,
+            ShopName: "Mark's",
+            CatalogItemId: items[i].CatalogItemId,
+            Name: items[i].Name,
+            Description: items[i].Description,
+            Manufacturer: items[i].Manufacturer,
+            Url: items[i].Url,
+            MobileUrl: items[i].MobileUrl,
+            ImageUrl: items[i].ImageUrl,
+            AdditionalImageUrls: items[i].AdditionalImageUrls,
+            CurrentPrice: items[i].CurrentPrice,
+            OriginalPrice: items[i].OriginalPrice,
+            DiscountPercentage: items[i].DiscountPercentage,
+            Currency: items[i].Currency,
+            StockAvailability: items[i].StockAvailability,
+            ExpirationDate: items[i].ExpirationDate,
+            Gtin: items[i].Gtin,
+            GtinType: items[i].GtinType,
+            Asin: items[i].Asin,
+            Mpn: items[i].Mpn,
+            Category:
+              typeof tax_item != "undefined" && tax_item.length > 0
+                ? tax_item[0].taxid
+                : "",
+            Colors: items[i].Colors,
+            Material: items[i].Material,
+            Size: items[i].Size,
+            SizeUnit: items[i].SizeUnit,
+            Condition: items[i].Condition,
+            AgeGroup: items[i].AgeGroup,
+            Gender: items[i].Gender,
+            Adult: items[i].Adult,
+            Uri: items[i].Uri,
+            Tag: [],
+          };
+          const prod = Maindb.collection("testProducts");
+          const inserted = await prod.insertOne(sendingdata);
+          console.log("Ending" + items[i].CatalogItemId);
+        }
+      });
+    });
+    run++;
+  }
+});
+
 module.exports = app;
